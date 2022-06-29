9a6e9482742bff043bf78634cf982d308bfa7add
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2022-06-29 15:31:15 -0300

    First commit

---
+++ include/rpg.h
@@ -0,0 +1,9 @@
+#ifndef RPG_H
+#define RPG_H
+
+int teste() {
+	int valor = 4;
+	return valor;
+}
+
+#endif
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "include/rpg.h"
+#include <stdlib.h>
+
+int main() {
+	printf("%d", teste());
+	return 0;
+}
